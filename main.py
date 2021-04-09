fc0d3586c244b85476cbfb21d7d29e75edc3490a
Lo <lkdsub0@gmail.com>
2021-04-09 09:36:47 +0200

    Added .env for POSTgres support on Heroku

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ## CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL1')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
